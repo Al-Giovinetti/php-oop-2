ef69e31ec86d52997a0d9d2ad83ed8fda52c07f0
Al-Giovinetti <giovinettialessio@gmail.com>
2023-07-14 17:34:56 +0200

    Prodotti  nel db amentati

--- db/db.php
+++ db/db.php
@@ -8,11 +8,12 @@ $categories = [
 ];
 
 $products = [
-    new Product("Crocchette",
-    "",
-    $categories[0],
-    "triangolari ripiene di pollo",
-    3.5)
+    new Product("Crocchette-Cat","",$categories[0],"triangolari ripiene di drago",3.50),
+    new Product("Canna con piume","",$categories[0],"canna in bambu marziano",10.00),
+    new Product("Tiragraffi","",$categories[0],"tappeto ipoallegenico",8.00),
+    new Product("Crocchette-dog","",$categories[1],"quadratini ripieni di torrone",5.30),
+    new Product("Frisbee","",$categories[1],"Cerchio volante",12.00),
+    new Product("Cuccia","",$categories[1],"casa abitabile",19.00)
 ]
 
 
